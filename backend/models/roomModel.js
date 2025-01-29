0de25c02f0440229ad3dc361956a5807411f8c67
Ashutosh Singh <singhashutosh1025@gmail.com>
2025-01-29 12:46:41 +0530

    roomSchema Done

---
+++ backend/models/roomModel.js
@@ -0,0 +1,28 @@
+const mongoose=require("mongoose");
+const{Schema}=mongoose;
+
+const roomSchema=new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    maxPeople:{
+        type:Number,
+        required:true,
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    roomNumber:[{number:Number,unAvailableDates:{type:[Date]}}],
+}
+,
+{timestamps:true}
+)
+
+module.exports=roomSchema;
